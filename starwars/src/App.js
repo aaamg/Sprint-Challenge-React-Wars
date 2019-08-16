e70b72551654f3c2d0b7c63918e888c78047c99e
Andrew García <52712672+aaamg@users.noreply.github.com>
2019-08-16 14:51:53 -0400

    Revert "Sprint-Challenge-React-Wars"

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import './App.css';
-import axios from "axios";
-import Comp from "./components/component";
-import Buttons from "./components/buttons";
-
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -13,50 +9,11 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [char, setChar] = useState([]);
-  useEffect(() => {
-  axios.get("https://swapi.co/api/people/")
-  .then(response => {
-    //console.log(response)
-    setChar(response.data.results);
-    console.log(char)
-    //setChar(starChar);
-    
-    
-  });
-}, []);
-
   return (
-    
-    
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {char.map((person, index) => {
-       return <Comp 
-                    name = {person.name} 
-                    height = {person.height}
-                    gender = {person.gender}
-                    birth_year = {person.birth_year}
-                    mass = {person.mass}
-                    key = {index}/>;
-                    
-      })}
-
-    <Buttons />
     </div>
   );
 }
 
 export default App;
-
-/*
-{char.map(person => {
-  return <Comp name = {person.name} gender = {person.gender}/>;
-})}
-
- {char.map((person, index) => {
-       return <Comp name = {person.name} key ={index}/>;
-      })}
-       
-
-*/
